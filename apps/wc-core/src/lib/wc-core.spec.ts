733622a83a5cf78ac79387ad8db0a1748656dbf5
Atanas-S-Nikolov <atanas.stoyanov1199@gmail.com>
2026-07-03 01:38:16 +0300

    Init nx workspace

---
+++ apps/wc-core/src/lib/wc-core.spec.ts
@@ -0,0 +1,7 @@
+import { wcCore } from './wc-core';
+
+describe('wcCore', () => {
+    it('should work', () => {
+        expect(wcCore()).toEqual('wc-core');
+    });
+});
---
+++ apps/wc-core/src/lib/wc-core.ts
@@ -0,0 +1,3 @@
+export function wcCore(): string {
+    return 'wc-core';
+}
